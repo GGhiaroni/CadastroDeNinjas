6b0d80113f5c6769e7ac2f908b0161b87964078d
GGhiaroni <gghiaronitiziano@gmail.com>
2026-03-04 21:56:49 -0300

    feat: Coloca o Lombok nas classes NinjaModel & ModelNinja

--- src/main/java/GabrielGhiaroni/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/GabrielGhiaroni/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,12 +1,18 @@
 package GabrielGhiaroni.CadastroDeNinjas.Ninjas;
 import GabrielGhiaroni.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -17,39 +23,6 @@ public class NinjaModel {
     @ManyToOne
     @JoinColumn(name = "missoes_id")
     private MissoesModel missoes;
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String email, String nome, int idade) {
-        this.email = email;
-        this.nome = nome;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
 
 
