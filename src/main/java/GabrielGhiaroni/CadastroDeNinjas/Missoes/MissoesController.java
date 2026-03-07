75ea8d3d101123e55a0668d61571986427b7d1a2
GGhiaroni <gghiaronitiziano@gmail.com>
2026-03-07 16:37:08 -0300

    feat: Atualiza o MissoesController com mock de dados

--- src/main/java/GabrielGhiaroni/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/GabrielGhiaroni/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,29 @@
 package GabrielGhiaroni.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
 public class MissoesController {
+
+    @GetMapping("/listar")
+    public String listarMissoes(){
+        return "Missões listadas com sucesso.";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missão criada com sucesso.";
+    }
+
+    @PutMapping("alterar/id")
+    public String alterarMissao(){
+        return "Missão alterada com sucesso.";
+    }
+
+    @DeleteMapping("deletar/id")
+    public String deletarMissao(){
+        return "Missão deletada com sucesso.";
+    }
 }
+
