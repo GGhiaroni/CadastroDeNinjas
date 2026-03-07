889329ad9c7dc2e76e606fbf7d18dee5a3f93c1a
GGhiaroni <gghiaronitiziano@gmail.com>
2026-03-07 16:11:17 -0300

    feat: Atualiza o NinjaController com mock de dados

--- src/main/java/GabrielGhiaroni/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/GabrielGhiaroni/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package GabrielGhiaroni.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -12,5 +10,32 @@ public class NinjaController {
     public String boasVindas(){
         return "Boas vindas ao sistema!";
     }
+
+    @GetMapping("/todos")
+    public String mostraTodosOsNinjas(){
+        return "Mostrar todos os ninjas.";
+    }
+
+    @GetMapping("/todos/id")
+    public String mostrarNinjaPorId(){
+        return "Mostrando ninja por ID.";
+    }
+
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "Ninja criado com sucesso.";
+    }
+
+    @PutMapping("/atualizar/id")
+    public String atualizarNinja(){
+        return "Ninja atualizado com sucesso.";
+    }
+
+    @DeleteMapping("/deletar/id")
+    public String deletarNinja(){
+        return "Ninja deletado com sucesso.";
+    }
+
+
 }
 
