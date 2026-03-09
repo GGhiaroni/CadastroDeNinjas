fe85db101c0e8e4ed47c31b2161661fa3e8d3145
GGhiaroni <gghiaronitiziano@gmail.com>
2026-03-09 11:39:38 -0300

    feat: Criando o NinjaRepository

---
+++ src/main/java/GabrielGhiaroni/CadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -0,0 +1,6 @@
+package GabrielGhiaroni.CadastroDeNinjas.Ninjas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
+}
