7122d8c0c9e018ab65dec950bac0e979a824c131
GGhiaroni <gghiaronitiziano@gmail.com>
2026-03-10 23:52:36 -0300

    chore: Inclui a versão do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos Entidade x DTO

--- src/main/java/GabrielGhiaroni/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/GabrielGhiaroni/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,22 @@
 package GabrielGhiaroni.CadastroDeNinjas.Ninjas;
 
+import GabrielGhiaroni.CadastroDeNinjas.Missoes.MissoesModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+    private Long id;
+    private String nome;
+    private String email;
+    private String imgUrl;
+    private int idade;
+    private String rank;
+    private MissoesModel missoes;
 }
+
+
