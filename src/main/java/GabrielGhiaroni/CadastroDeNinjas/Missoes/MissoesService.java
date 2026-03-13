9c187679e309b9b1ba56a6bc779e97aa8093248d
GGhiaroni <gghiaronitiziano@gmail.com>
2026-03-13 09:05:38 -0300

    feat: Inicia o desenvolvimento do MissoesService e injeta a dependência do MissoesRepository

--- src/main/java/GabrielGhiaroni/CadastroDeNinjas/Missoes/MissoesService.java
+++ src/main/java/GabrielGhiaroni/CadastroDeNinjas/Missoes/MissoesService.java
@@ -1,4 +1,9 @@
 package GabrielGhiaroni.CadastroDeNinjas.Missoes;
 
 public class MissoesService {
+    private final MissoesRepository missoesRepository;
+
+    public MissoesService(MissoesRepository missoesRepository) {
+        this.missoesRepository = missoesRepository;
+    }
 }
