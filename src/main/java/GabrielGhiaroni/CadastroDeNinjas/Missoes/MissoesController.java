d82ce03f96fd99433172d9d3d7966c5981240549
GGhiaroni <gghiaronitiziano@gmail.com>
2026-03-13 10:24:32 -0300

    feat: Implementa todos os endpoints para inserção, deleção, atualização e listagem de missões. Já funcionando.

--- src/main/java/GabrielGhiaroni/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/GabrielGhiaroni/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,34 +1,41 @@
 package GabrielGhiaroni.CadastroDeNinjas.Missoes;
-
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("missoes")
 public class MissoesController {
 
-    @GetMapping("/listar")
-    public String listarMissoes(){
-        return "Missões listadas com sucesso.";
+    private final MissoesService missoesService;
+
+    public MissoesController(MissoesService missoesService) {
+        this.missoesService = missoesService;
     }
 
-    @GetMapping("/id")
-    public String mostrarMissao(){
-        return "Missão mostrada com sucesso.";
+    @GetMapping("/listar")
+    public List<MissoesModel> listarMissoes(){
+        return missoesService.listarMissoes();
+    }
+
+    @GetMapping("/listar/{id}")
+    public MissoesModel listarMissaoPorId(@PathVariable Long id){
+        return missoesService.listarMissaoPorId(id);
     }
 
     @PostMapping("/criar")
-    public String criarMissao(){
-        return "Missão criada com sucesso.";
+    public MissoesModel criarMissao(@RequestBody MissoesModel missao){
+        return missoesService.criarMissao(missao);
     }
 
-    @PutMapping("atualizar/id")
-    public String atualizarMissao(){
-        return "Missão alterada com sucesso.";
+    @PutMapping("/atualizar/{id}")
+    public MissoesModel atualizarMissao(@PathVariable Long id, @RequestBody MissoesModel missaoAtualizada){
+        return missoesService.atualizarMissao(id, missaoAtualizada);
     }
 
-    @DeleteMapping("deletar/id")
-    public String deletarMissao(){
-        return "Missão deletada com sucesso.";
+    @DeleteMapping("/deletar/{id}")
+    public void deletarMissao(@PathVariable Long id){
+        missoesService.deletarMissao(id);
     }
 }
 
--- src/main/java/GabrielGhiaroni/CadastroDeNinjas/Missoes/MissoesService.java
+++ src/main/java/GabrielGhiaroni/CadastroDeNinjas/Missoes/MissoesService.java
@@ -1,9 +1,42 @@
 package GabrielGhiaroni.CadastroDeNinjas.Missoes;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MissoesService {
     private final MissoesRepository missoesRepository;
 
     public MissoesService(MissoesRepository missoesRepository) {
         this.missoesRepository = missoesRepository;
+    }
+
+    public List<MissoesModel> listarMissoes() {
+        return missoesRepository.findAll();
+    }
+
+    public MissoesModel listarMissaoPorId(Long id) {
+        Optional<MissoesModel> missaoEncontrada = missoesRepository.findById(id);
+        return missaoEncontrada.orElse(null);
+    }
+
+    public MissoesModel criarMissao(MissoesModel missao) {
+        return missoesRepository.save(missao);
+    }
+
+    public MissoesModel atualizarMissao(Long id, MissoesModel missao) {
+        Optional<MissoesModel> missaoEncontrada = missoesRepository.findById(id);
+        if (missaoEncontrada.isPresent()) {
+            missao.setId(id);
+            return missoesRepository.save(missao);
+        } else {
+            return null;
+        }
+    }
+
+    public void deletarMissao(Long id) {
+        missoesRepository.deleteById(id);
     }
 }
